080dcce4a2b65a961a8988df0f64b06a0658425a
gustavocadev <gustavocadev@gmail.com>
2023-08-05 11:37:30 -0500

    Add changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,33 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import {
+  type DocumentHead,
+  Form,
+  routeAction$,
+  zod$,
+  z,
+} from '@builder.io/qwik-city';
+
+export const useAddQuery = routeAction$(
+  async (values, event) => {
+    const url = new URL('/', event.url.origin);
+
+    url.searchParams.set('q', values.q);
+
+    console.log('url', url.search);
+    console.log('url', url.href);
+
+    // Not working any of these, I expect to be redirected to /?q=foo, but the url doesnt change
+    // I noticed that only when I add the query doesnt work, because it works with normal routes
+    // throw event.redirect(303, `/${url.search}`);
+    // throw event.redirect(303, url.href);
+  },
+  zod$({
+    q: z.string(),
+  })
+);
 
 export default component$(() => {
+  const addQuery = useAddQuery();
   return (
     <>
       <h1>Hi 👋</h1>
@@ -10,16 +36,23 @@ export default component$(() => {
         <br />
         Happy coding.
       </p>
+      <Form action={addQuery}>
+        <label for="">
+          <input type="text" name="q" />
+        </label>
+
+        <button type="submit">Add query</button>
+      </Form>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Welcome to Qwik',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: 'description',
+      content: 'Qwik site description',
     },
   ],
 };
---
+++ src/routes/shira/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <>
+      <h2>Shira!</h2>
+    </>
+  );
+});
